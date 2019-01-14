05496533cfd345602c5fba99574e5a93cfbc562a
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-14 16:28:42 +0100

    correction

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
-﻿function pushMe()
+﻿// Déclaration de la fonction affichant une boite de dialogue quand on appuie sur une touche 
+function pushMe()
 {
+    // On récupère ce qui est saisie dans l'input
     var name = document.getElementById("lastname").value;
     alert(name);
 }
